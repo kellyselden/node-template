e67b3d389c8efd14225a1817d8bb5c3e44943c6f
Kelly Selden <kellyselden@gmail.com>
2019-12-30 23:16:49 +0000

    add readme

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  locals(options) {
+    let { name } = options.entity;
+
+    let encodedName = name.replace(/@/g, '%40').replace(/\//g, '%2F');
+
+    return {
+      name,
+      encodedName
+    };
+  }
 };
