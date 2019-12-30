8bc29d56449c9ee2c85a614f57ca8934135c1601
Kelly Selden <kellyselden@gmail.com>
2019-12-30 18:38:14 +0000

    add .gitignore

---
+++ files/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  env: {
+    es6: true
+  },
+  extends: [
+    'sane-node'
+  ],
+  overrides: [
+    {
+      files: [
+        'test/**/*-test.js'
+      ],
+      env: {
+        mocha: true
+      },
+      plugins: [
+        'mocha'
+      ],
+      extends: [
+        'plugin:mocha/recommended'
+      ],
+      rules: {
+        'mocha/no-exclusive-tests': 'error'
+      }
+    }
+  ]
+};
--- test/index-test.js
+++ test/index-test.js
@@ -4,11 +4,12 @@ const { describe } = require('./helpers/mocha');
 const { expect } = require('./helpers/chai');
 const execa = require('execa');
 const path = require('path');
-const {
-  buildTmp
-} = require('git-fixtures');
-
-const blueprintPath = path.resolve(__dirname, '..');
+const { buildTmp } = require('git-fixtures');
+const { promisify } = require('util');
+const getTmpDir = promisify(require('tmp').dir);
+const gitInit = require('git-diff-apply/src/git-init');
+const copy = require('git-diff-apply/src/copy');
+const commitAndTag = require('git-diff-apply/src/commit-and-tag');
 
 function ecu(args, options) {
   let ps = execa('ember-cli-update', args, {
@@ -26,6 +27,14 @@ function ecu(args, options) {
 describe(function() {
   this.timeout(10 * 1000);
 
+  before(async function() {
+    this.blueprintPath = await getTmpDir();
+
+    await gitInit({ cwd: this.blueprintPath });
+    await copy(path.resolve(__dirname, '..'), this.blueprintPath);
+    await commitAndTag('v0.0.0', { cwd: this.blueprintPath });
+  });
+
   beforeEach(async function() {
     this.tmpPath = await buildTmp({
       fixturesPath: path.resolve(__dirname, 'fixtures')
@@ -36,12 +45,12 @@ describe(function() {
     await ecu([
       'init',
       '-b',
-      blueprintPath
+      this.blueprintPath
     ], {
       cwd: this.tmpPath
     });
 
-    expect(path.join(this.tmpPath, 'renovate.json'))
+    expect(path.join(this.tmpPath, '.gitignore'))
       .to.be.a.file();
   });
 });
