1bfc74e6505b26ef354c4d915f4530fe2e081319
Kelly Selden <kellyselden@gmail.com>
2020-01-19 12:05:00 +0000

    update tests

--- test/fixtures/expected/README.md
+++
@@ -1,3 +0,0 @@
-# @my-scope/my-project
-
-[![npm version](https://badge.fury.io/js/%40my-scope%2Fmy-project.svg)](https://badge.fury.io/js/%40my-scope%2Fmy-project)
--- test/index-test.js
+++ test/index-test.js
@@ -2,55 +2,38 @@
 
 const { describe } = require('./helpers/mocha');
 const { expect } = require('./helpers/chai');
-const execa = require('execa');
 const path = require('path');
-const { buildTmp } = require('git-fixtures');
-const { promisify } = require('util');
-const getTmpDir = promisify(require('tmp').dir);
-const gitInit = require('git-diff-apply/src/git-init');
-const copy = require('git-diff-apply/src/copy');
-const commitAndTag = require('git-diff-apply/src/commit-and-tag');
+const {
+  emberInit: _emberInit,
+  setUpBlueprintMocha
+} = require('ember-cli-update-test-helpers');
 
-function ecu(args, options) {
-  let ps = execa('ember-cli-update', args, {
-    preferLocal: true,
-    localDir: __dirname,
-    stdio: ['pipe', 'pipe', 'inherit'],
-    ...options
+async function emberInit({
+  args = []
+}) {
+  return await _emberInit({
+    args: [
+      '-sn',
+      ...args
+    ]
   });
-
-  ps.stdout.pipe(process.stdout);
-
-  return ps;
 }
 
 describe(function() {
-  this.timeout(10 * 1000);
+  this.timeout(5 * 1000);
 
-  before(async function() {
-    this.blueprintPath = await getTmpDir();
-
-    await gitInit({ cwd: this.blueprintPath });
-    await copy(path.resolve(__dirname, '..'), this.blueprintPath);
-    await commitAndTag('v0.0.0', { cwd: this.blueprintPath });
-  });
-
-  beforeEach(async function() {
-    this.tmpPath = await buildTmp({
-      fixturesPath: path.resolve(__dirname, 'fixtures/base')
-    });
-  });
+  // eslint-disable-next-line mocha/no-setup-in-describe
+  setUpBlueprintMocha.call(this);
 
   it('works', async function() {
-    await ecu([
-      'init',
-      '-b',
-      this.blueprintPath
-    ], {
-      cwd: this.tmpPath
+    let cwd = await emberInit({
+      args: [
+        '-b',
+        this.blueprintPath
+      ]
     });
 
-    expect(path.join(this.tmpPath, 'renovate.json'))
+    expect(path.join(cwd, 'renovate.json'))
       .to.be.a.file();
   });
 });
